002bb1df8bfa671094399b4c404d83b056f737c4
DOXOPOKC <lilondin@gmail.com>
2021-06-01 22:02:25 +0700

    add parcel && pug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
  
-app.use(express.static('./'));
+app.use(express.static('./dist'));
  
 app.listen(PORT, () => {
   console.log(`Мой текст и порт: ${PORT}!`);
