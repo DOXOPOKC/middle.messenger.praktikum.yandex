fe0db278e993d4ba5af9e7bed5deaabac61d5525
skorokhod <d.skorokhod@cft.ru>
2021-06-19 12:14:56 +0700

    Revert "test"
    This reverts commit 6c6d1e53

--- src/components/button/template.ts
+++
@@ -1,4 +0,0 @@
-export const template = `
-    div(class=className)
-        != child
-`;
--- src/utils/renderDOM.ts
+++
@@ -1,9 +0,0 @@
-// import Block from '../modules/block';
-
-export function render(query: string, block: any) {
-    // const root = document.querySelector(query);
-
-    // root.appendChild(block.getContent());
-
-    // return root;
-}
