2c5119f2955c2602e65a62f38578339f93c7da1c
skorokhod <d.skorokhod@cft.ru>
2021-06-23 23:20:23 +0700

    Добавил скелет профиля

---
+++ src/pages/profile-password/index.ts
@@ -0,0 +1,49 @@
+import { compile } from "pug";
+import Block, { renderBlock } from '../../core/block';
+
+const source = `
+aside.sidebar.sidebar_mini
+  .sidebar__button
+    .rounded-button
+      include ../../../static/icons/arrow-left.svg
+main.content
+  .profile
+    .profile__avatar-wrapper.avatar
+      .profile__change-avatar
+        .blur-background
+        span.text_light.avatar__text Поменять аватар
+      .profile__avatar
+        include ../../../static/icons/image.svg
+    .profile__title.title Иван
+    .profile__main
+      +form({ firstBtn: false, secondBtn: false, isRow: true })
+    .profile__actions
+      button.profile__button.button.text-light Сохранить
+`;
+
+const template = compile(source);
+
+class Profile extends Block {
+  constructor() {
+    super('div', {
+      classNames: 'profile',
+      events: {
+        click: (e: Event) => this.handleClick(e)
+      }
+    });
+  }
+
+  handleClick(e: Event) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  render() {
+    return template(this.props);
+  }
+}
+
+
+const page = new Profile();
+
+renderBlock('.app', page);
